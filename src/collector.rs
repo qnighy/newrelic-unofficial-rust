c08ef5c9bd4a0861c36445cb13de42372de9ee7a
Masaki Hara <ackie.h.gmai@gmail.com>
2020-07-05 19:24:44 +0900

    Handle errors in preconnection

--- src/collector.rs
+++ src/collector.rs
@@ -3,6 +3,8 @@ use flate2::write::GzEncoder;
 use flate2::Compression;
 use serde::{Deserialize, Serialize};
 
+const MAX_PAYLOAD_SIZE: usize = 1000 * 1000;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct PreconnectRequest {
     #[serde(default, skip_serializing_if = "String::is_empty")]
@@ -11,6 +13,33 @@ struct PreconnectRequest {
 }
 
 pub(crate) fn connect_attempt(license: &str) -> anyhow::Result<()> {
+    collector_request_internal(Request {
+        method: "preconnect",
+        host: "collector.newrelic.com",
+        run_id: None,
+        max_payload_size: MAX_PAYLOAD_SIZE,
+        license: license,
+        data: &vec![PreconnectRequest {
+            security_policies_token: "".to_owned(),
+            high_security: false,
+        }],
+    })?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Request<'a, T> {
+    method: &'a str,
+    host: &'a str,
+    run_id: Option<&'a str>,
+    max_payload_size: usize,
+    license: &'a str,
+    // request_headers_map: HashMap<String, String>,
+    data: &'a T,
+}
+
+fn collector_request_internal<T: Serialize>(req: Request<'_, T>) -> anyhow::Result<()> {
     let compressed = {
         let mut stream = GzEncoder::new(Vec::<u8>::new(), Compression::default());
         serde_json::to_writer(
@@ -23,12 +52,20 @@ pub(crate) fn connect_attempt(license: &str) -> anyhow::Result<()> {
         .unwrap();
         stream.finish().unwrap()
     };
-    eprintln!("compressed = {:?}", compressed);
-    let resp = attohttpc::post("https://collector.newrelic.com/agent_listener/invoke_raw_method")
+    anyhow::ensure!(
+        compressed.len() < req.max_payload_size,
+        "Payload size for {} too large: {} greater than {}",
+        req.method,
+        compressed.len(),
+        req.max_payload_size
+    );
+
+    let url = format!("https://{}/agent_listener/invoke_raw_method", req.host);
+    let resp = attohttpc::post(url)
         .param("marshal_format", "json")
         .param("protocol_version", "17")
         .param("method", "preconnect")
-        .param("license_key", license)
+        .param("license_key", req.license)
         .header("Accept-Encoding", "identity, deflate")
         .header("Content-Type", "application/octet-stream")
         .header("User-Agent", "NewRelic-Rust-Agent-Unofficial/0.1.0")
@@ -36,9 +73,12 @@ pub(crate) fn connect_attempt(license: &str) -> anyhow::Result<()> {
         .body(Bytes(compressed))
         .send()?;
 
-    eprintln!("resp = {:#?}", resp);
-    let body = resp.text()?;
-    eprintln!("body = {:?}", body);
+    anyhow::ensure!(
+        [200, 202].contains(&resp.status().as_u16()),
+        "response code: {}",
+        resp.status().as_u16(),
+    );
+    eprintln!("resp = {:?}", resp);
 
     Ok(())
 }
